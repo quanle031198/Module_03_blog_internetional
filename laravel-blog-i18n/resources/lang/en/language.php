4c9e43cc307aecaeba911ddf90854a5257483151
quanle031198 <bluestargaming000@gmail.com>
2021-05-31 09:07:49 +0700

    laravel blog i18n

---
+++ laravel-blog-i18n/resources/lang/en/language.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'blog_application' => 'Blog Application',
+    'home' => 'Home',
+    'list_post' => 'Post list',
+    'create_post' => 'Create post', 
+    'save' => 'Save',
+    'post_not_exist' => 'post not exist',
+    'title' => 'Title',
+    'content' => 'Content'
+
+];
